f565f72e35a11cf8abca44866e02befd0c9444d1
LeeYooBin <santosroberdan25@gmail.com>
2024-03-11 21:40:15 -0300

    Resolvendo teste de lógica

---
+++ src/fibonacci.js
@@ -0,0 +1,14 @@
+const fib = (numero, a = 0, b = 1) => {
+    if (a === numero || b === numero) return true;
+    else if (b > numero) return false;
+    return fib(numero, b, a + b);
+}
+
+console.log(fib(8));
+console.log(fib(10));
+console.log(fib(13));
+console.log(fib(21));
+console.log(fib(12));
+console.log(fib(34));
+console.log(fib(55));
+console.log(fib(9));
